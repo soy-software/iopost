7413f33e1f61576520235971ccf8e22a87e3469d
soy-software <soysoftware@gmail.com>
2019-11-08 17:30:56 -0500

    pdf incripcion

--- resources/views/inscripciones/inscripcionPdf.blade.php
+++ resources/views/inscripciones/inscripcionPdf.blade.php
@@ -22,6 +22,24 @@
     </style>
 </head>
 <body>
+        <table style="border-collapse: collapse; border: none;">
+            <td class="noBorder" style="text-align: center;">
+                <img src="{!! public_path('img/utc.png') !!}" alt="" width="125px;" style="text-align: right;">
+                <h4>
+                    UNIVERSIDAD TÉCNICA DE COTOPAXI
+                </h4>
+                <small>Formulario de inscripción # {{ $inscripcion->id }}</small>
+            </td>
+        </table>
     @include('inscripciones.info')
+    <p>
+        <strong>NOTA 1: </strong>Por favor, acerquese con este documento a la secretaría de posgrados a realizar el pago correspondiente de la inscripción.
+    </p>
+    <p>
+        <strong>NOTA 2:</strong> O puede subir le voucher del deposito en la siguente url:  <a href="">{{ route('subirComprobantePago',$inscripcion->id) }}</a>
+    </p>
+    <p>
+        <strong>NOTA 3:</strong> Cuentas de Bancos Pichincha
+    </p>
 </body>
 </html>

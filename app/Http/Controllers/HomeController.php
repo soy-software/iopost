f13f65cb7ac6afd98fb91b01ccb673c9fc028436
soy-software <soysoftware@gmail.com>
2019-11-25 09:42:28 -0500

    envio de correciones

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -43,40 +43,40 @@ class HomeController extends Controller
     // D:actualizar datos de mi perfil
     public function miPerfilActualizarDatos(RqActualizarDatosPerfil $request)
     {
-        // $user=Auth::user();
-        // if($request->password){
-        //     $user->password=Hash::make($request->password);
-        // }
-        // $user->nombres=$request->nombres;
-        // $user->apellidos=$request->apellidos;
-        // $user->sexo=$request->sexo;
-        // $user->tipo_identificacion=$request->tipo_identificacion;
-        // $user->identificacion=$request->identificacion;
-        // $user->fecha_nacimiento=$request->fecha_nacimiento;
-        // $user->estado_civil=$request->estado_civil;
-        // $user->etnia=$request->etnia;
-        // $user->telefono=$request->telefono;
-        // $user->celular=$request->celular;
-        // $user->pais=$request->pais;
-        // $user->parroquia_id=$request->parroquia;
-        // $user->direccion=$request->direccion;
-        // $user->tiene_discapacidad=$request->tiene_discapacidad;
-        // $user->porcentaje_discapacidad=$request->porcentaje_discapacidad??0;
-        // $user->tiene_carnet_conadis=$request->tiene_carnet_conadis;
-        // $user->porcentaje_carnet_conadis=$request->porcentaje_carnet_conadis??0;
-        // $user->save();
+        $user=Auth::user();
+        if($request->password){
+            $user->password=Hash::make($request->password);
+        }
+        $user->nombres=$request->nombres;
+        $user->apellidos=$request->apellidos;
+        $user->sexo=$request->sexo;
+        $user->tipo_identificacion=$request->tipo_identificacion;
+        $user->identificacion=$request->identificacion;
+        $user->fecha_nacimiento=$request->fecha_nacimiento;
+        $user->estado_civil=$request->estado_civil;
+        $user->etnia=$request->etnia;
+        $user->telefono=$request->telefono;
+        $user->celular=$request->celular;
+        $user->pais=$request->pais;
+        $user->parroquia_id=$request->parroquia;
+        $user->direccion=$request->direccion;
+        $user->tiene_discapacidad=$request->tiene_discapacidad;
+        $user->porcentaje_discapacidad=$request->porcentaje_discapacidad??0;
+        $user->tiene_carnet_conadis=$request->tiene_carnet_conadis;
+        $user->porcentaje_carnet_conadis=$request->porcentaje_carnet_conadis??0;
+        $user->save();
 
-        // if ($request->hasFile('foto')) {
-        //     if ($request->file('foto')->isValid()) {
-        //         Storage::delete($user->foto);
-        //         $extension = $request->foto->extension();
-        //         $path = Storage::putFileAs(
-        //             'public/usuarios', $request->file('foto'), $user->id.'.'.$extension
-        //         );
-        //         $user->foto=$path;
-        //         $user->save();
-        //     }
-        // }
+        if ($request->hasFile('foto')) {
+            if ($request->file('foto')->isValid()) {
+                Storage::delete($user->foto);
+                $extension = $request->foto->extension();
+                $path = Storage::putFileAs(
+                    'public/usuarios', $request->file('foto'), $user->id.'.'.$extension
+                );
+                $user->foto=$path;
+                $user->save();
+            }
+        }
 
         $request->session()->flash('success','Usuario actualizado');
         
